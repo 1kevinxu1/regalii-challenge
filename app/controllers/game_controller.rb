0a52f79fac0da68b16bffe036d481f811975e99b
Kevin Xu <1kevinxu1@gmail.com>
2017-04-02 19:46:56 -0400

    successfully create games, display errors on failure, and success message on creation

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,4 +1,18 @@
 class GameController < ApplicationController
-  
+  def create
+    @game = Game.new(game_params.merge(player_one_id: current_user.id))
+    if @game.save
+      redirect_to log_url
+      flash[:notice] = "Game successfully logged"
+    else
+      flash.now[:errors] = @game.errors.full_messages
+      render "home/log"
+    end
+  end
 
+  private
+
+  def game_params
+    params.require(:game).permit(:date_played, :player_one_score, :player_two_score, :player_two_id)
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -5,12 +5,13 @@ class Game < ActiveRecord::Base
   belongs_to :player_two, class_name: "User"
 
   def date_played_cannot_be_in_the_future
-    if date_played > Date.today
+    if date_played && date_played > Date.today
       errors.add(:date_played, "can't be in the future")
     end
   end
 
   def score_differential_at_least_two
+    return unless player_one_score && player_two_score
     score_diff = player_one_score - player_two_score
     score_diff *= -1 if score_diff < 0
     if score_diff < 2
@@ -19,8 +20,13 @@ class Game < ActiveRecord::Base
   end
 
   def game_must_be_played_to_21
+    return unless player_one_score && player_two_score
     if player_one_score < 21 && player_two_score < 21
       errors.add(:winning_score, "must be at least 21")
+    end
+    if player_one_score > 19 && (player_two_score != player_one_score + 2 &&
+                                 player_two_score != player_one_score - 2)
+      errors.add(:games, "must be played to 21")
     end
   end
 end
