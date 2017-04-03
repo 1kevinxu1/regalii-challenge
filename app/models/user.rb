7dc9f5beaf70ba874d6310e484fc97b59099a256
Kevin Xu <1kevinxu1@gmail.com>
2017-04-02 20:52:18 -0400

    Fix rank display based on elo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ActiveRecord::Base
   validates :elo, presence: true
   devise :database_authenticatable, :registerable, :trackable, :validatable
+  attr_accessor :rank
+
+  def self.get_all_by_rank
+    rank = 1
+    cur_elo = nil
+    user_by_rank = User.all.order("elo DESC").each do |user|
+      rank += 1 if cur_elo && user.elo < cur_elo
+      cur_elo = user.elo
+      user.rank = rank
+    end
+    user_by_rank
+  end
 
   def games
     Game.where("player_one_id = ? OR player_two_id = ?", self.id, self.id).order('created_at DESC')
